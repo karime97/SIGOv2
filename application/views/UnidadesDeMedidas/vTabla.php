636421727cfc4d6e8c3b6e4a579f3239d5b14b6f
karime <keyla9873@gmail.com>
2019-07-01 18:38:01 -0500

    Update vTabla.php

---
+++ application/views/UnidadesDeMedidas/vTabla.php
@@ -0,0 +1,60 @@
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered display" style="width:100%" id="grid">
+                        <thead>
+                            <tr>
+                                <th>Clave</th>
+                                <th>Unidad Básica de Presuspuestación (UBP)</th>
+                                <th>Año</th>
+                                <th width="150px">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                            foreach($consulta as $value){ ?>
+                            <tr>
+                                <td><?= $value->vClave ?></td>
+                                <td><?= $value->vUBP ?></td>
+                                <td><?= $value->iAnio ?></td>
+                                <td>
+                                    <button type="button" class="btn btn-circle waves-effect waves-light btn-warning " onclick="modificarUBP(<?= $value->iIdUbp ?>)"><i class="mdi mdi-border-color" ></i></button>
+                                    <button type="button" class="btn btn-circle waves-effect waves-light btn-danger " onclick="confirmar('¿Está seguro de eliminar?', eliminarUBP,<?= $value->iIdUbp ?>);"><i class="mdi mdi-close"></i></button>
+                                </td>
+                             </tr>
+                             <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="holi"></div>
+
+<script>
+$(document).ready(function () {
+        $('#grid').DataTable();
+    });
+
+        function eliminarUBP(key){
+                                $.ajax({
+                                    type: "POST",
+                                    url: "<?= base_url() ?>C_ubps/eliminar",
+                                    data: {'key' : key},
+                                    //contentType: 'json',
+                                    success: function(resp){
+                                        if(resp == true){
+                                            cargar('<?= base_url()?>C_ubps/regresar','#contenido_modulo');
+                                        }else{
+                                            alert(resp);
+                                        }
+                                    },
+                                    error: function(XMLHHttRequest, textStatus, errorThrown) {
+                                        
+                                    }
+                                });
+                            }
+</script>
