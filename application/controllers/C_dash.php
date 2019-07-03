52690ce4ace932eff633b33f4ea6ac649195bf00
LitsoCr <randy_cruzh@hotmail.com>
2019-07-03 03:37:22 -0500

    Se agrego la libreria highmaps, se realizo el maquetado de dash/dashboard.php, se agregaron las graficas en la tabla

---
+++ application/controllers/C_dash.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class C_dash extends CI_Controller {
+	public function __construct()
+    {
+        parent::__construct();
+        session_start();
+        $this->load->helper('url');
+        //$this->load->model('');
+        //$this->load->library('Class_options');
+        //$this->load->model('M_seguridad','ms');
+    }
+
+    public function index()
+    {
+        $this->load->view('dash/dashboard');
+        //$this->load->view('dash/mapa');
+
+    }
+}
